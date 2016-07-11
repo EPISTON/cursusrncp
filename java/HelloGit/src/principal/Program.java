a966f77ed30841dc6a9d1a7d743911d67008cf7b
vincent courtalon <vincent.courtalon@gmail.com>
2016-07-11 14:27:35 +0200

    classes et exercice echec

--- java/HelloGit/src/principal/Program.java
+++ java/HelloGit/src/principal/Program.java
@@ -4,8 +4,12 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("bonjour git");
-		System.out.println("et bon appétit");
+		System.out.println("et bon appï¿½tit");
 
+		IHello var1;
+		var1 = new MyGreeting();
+		var1.greetings("bob");
+		
 	}
 
 }
