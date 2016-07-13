7af30e8f49431d1597330452980e830d44da7992
vincent courtalon <vincent.courtalon@gmail.com>
2016-07-13 09:40:47 +0200

    correction bonus exercice 1 de java

--- java/javaExercice1_3Form/src/javaExercice1_3Form/Program.java
+++ java/javaExercice1_3Form/src/javaExercice1_3Form/Program.java
@@ -22,6 +22,32 @@ public class Program {
 			System.out.println();
 		}
 		
+		// dessin du losange
+		int taille_demi = taille / 2 + 1;
+		String demi_losange_haut = "";
+		String demi_losange_bas = "";
+		
+		for (int ligne = 0; ligne < taille_demi; ligne++) {
+			String ligne_courante = "";
+			
+			// génération du retrait/marge
+			for (int colonne = 0; colonne <taille_demi - ligne - 1; colonne++)
+				ligne_courante += " ";
+			
+			// génération du corps du losange
+			for (int colonne = 0; colonne < ligne * 2 + 1; colonne++) {
+				if (colonne == 0 || colonne == ligne * 2)
+					ligne_courante += "*";
+				else
+					ligne_courante += "-";
+			}
+			ligne_courante += "\n";
+			demi_losange_haut = demi_losange_haut + ligne_courante;
+			if (ligne < taille_demi - 1)
+				demi_losange_bas = ligne_courante + demi_losange_bas;
+		}
+		System.out.println(demi_losange_haut + demi_losange_bas);
+		
 	
 	}
 
