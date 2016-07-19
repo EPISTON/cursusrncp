0c044be16a4ad127328e16f46f4b20fec8d57f6c
vincent courtalon <vincent.courtalon@gmail.com>
2016-07-19 14:53:02 +0200

    illustration des layouts basiques

---
+++ java/javaSwing2/src/javaSwing2/Program.java
@@ -0,0 +1,11 @@
+package javaSwing2;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MicroOnde mc = new MicroOnde();
+		mc.setVisible(true);
+	}
+
+}
