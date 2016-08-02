a6783f6168d5c75ebe010e1a6a1ec868cd28815f
vincent courtalon <vincent.courtalon@gmail.com>
2016-08-02 12:15:07 +0200

    ajout configuration indépendante par instance de servlet

--- javaweb/myLoginForm/src/myLoginForm/servlets/CheckLoginServlet.java
+++ javaweb/myLoginForm/src/myLoginForm/servlets/CheckLoginServlet.java
@@ -1,6 +1,10 @@
 package myLoginForm.servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,12 +16,39 @@ import javax.servlet.http.HttpServletResponse;
 public class CheckLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private Map<String, String> credentials;
+	
     /**
      * Default constructor. 
      */
     public CheckLoginServlet() {
         // TODO Auto-generated constructor stub
     }
+    
+    
+
+    // cette méthode est appelé automatiquement par tomcat
+    // une fois apres la constrcution de la servlet
+    // avec en parametre toute la configuration de celle-ci
+    @Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		credentials = new HashMap<String, String>();
+		String[] logins = config.getInitParameter("logins").split(";");
+		String[] passwords = config.getInitParameter("passwords").split(";");
+		for (int i = 0; i < logins.length; i++) {
+			credentials.put(logins[i], passwords[i]);
+		}
+	}
+
+    private boolean check_credentials(String login, String password) {
+    	if (credentials.containsKey(login) && credentials.get(login).equals(password)) 
+    		return true;
+    	else
+    		return false;
+    }
+
+    
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
@@ -32,7 +63,13 @@ public class CheckLoginServlet extends HttpServlet {
 		if (nom == null || password == null) {
 			getServletContext().getRequestDispatcher("/denied.jsp").forward(request, response);
 		}
-		// si le login et mot de passe sont correct -> accès autorisé
+		else if (check_credentials(nom, password)) {
+			getServletContext().getRequestDispatcher("/success.jsp").forward(request, response);
+		}
+		else {
+			getServletContext().getRequestDispatcher("/denied.jsp").forward(request, response);
+		}
+	/*	// si le login et mot de passe sont correct -> accès autorisé
 		else if (nom.equals("bob") && password.equals("1234")) {
 			getServletContext().getRequestDispatcher("/success.jsp").forward(request, response);
 		}
@@ -40,7 +77,7 @@ public class CheckLoginServlet extends HttpServlet {
 		else {
 			getServletContext().getRequestDispatcher("/denied.jsp").forward(request, response);
 		}
-		
+		*/
 		
 	}
 
