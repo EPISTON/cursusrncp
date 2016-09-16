4c82aa36b50740e2d3bc082da26d67be609974c7
vincent courtalon <vincent.courtalon@gmail.com>
2016-09-16 14:59:17 +0200

    ajout des vues angular avec ng-include

--- javascript/produitBioForm/src/main/java/com/courtalon/produitBioForm/repositories/ProduitRepository.java
+++ javascript/produitBioForm/src/main/java/com/courtalon/produitBioForm/repositories/ProduitRepository.java
@@ -1,6 +1,7 @@
 package com.courtalon.produitBioForm.repositories;
 
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.courtalon.produitBioForm.metier.Produit;
