f226f187cf58f5e69254f5a71133dc7660cd4175
vincent.courtalon <vincent.courtalon@gmail.com>
2016-09-29 09:51:53 +0200

    consommation d'un service externe depuis apachecxf et spring

--- webservices/MyMessageApp/src/main/java/com/courtalon/MyMessageApp/actions/IndexAction.java
+++ webservices/MyMessageApp/src/main/java/com/courtalon/MyMessageApp/actions/IndexAction.java
@@ -4,12 +4,23 @@ import java.util.Date;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.courtalon.myContactService.ws.ContactService;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class IndexAction extends ActionSupport {
 	
 	private static Logger log = LogManager.getLogger(ActionSupport.class); 
 	private static final long serialVersionUID = 1L;
+
+	// le service externe auquel je veux acceder
+	private ContactService contactService;
+	public ContactService getContactService() {
+		return contactService;
+	}
+	public void setContactService(ContactService contactService) {
+		this.contactService = contactService;
+	}
+
 
 	private String message;
 	
@@ -23,7 +34,7 @@ public class IndexAction extends ActionSupport {
 
 	public String index() {
 		log.info("appel de index!");
-		message = "bonjour depuis index le " + new Date();
+		message = "bonjour depuis index le " + contactService.findByID(1).getNom();
 		return SUCCESS;
 	}
 
