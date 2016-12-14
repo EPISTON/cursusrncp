ec1531f994248252211f57643653c8bcc071a2bc
vincent courtalon <vincent.courtalon@gmail.com>
2016-12-14 07:54:37 +0100

    ajout du client angularjs pour giga galerie v2

---
+++ decembre/gigaGalerie2/gigaGalerieClient/js/galerieApp.js
@@ -0,0 +1,27 @@
+/**
+ *  giga galerie angular app
+ */
+
+var app = angular.module("galerieApp", ["ui.bootstrap", "ngFileUpload", "ngCookies", "bootstrapLightbox", "ngSanitize", "angular.filter"]);
+
+app.constant("serverUrl", "http://localhost:8080/gigaMvcGalerie")
+    .constant("imageUrl", "/rest/images")
+    .constant("licenseUrl", "/rest/licenses")
+    .constant("assetSourceUrl", "/rest/assetsources")
+    .constant("tagUrl", "/rest/tags");
+
+app.config(function (imageServiceProvider,
+    tagServiceProvider,
+    licenseAndSourceServiceProvider,
+    serverUrl,
+    imageUrl,
+    licenseUrl,
+    assetSourceUrl,
+    tagUrl) {
+
+
+    imageServiceProvider.setServiceUrl(serverUrl + imageUrl);
+    licenseAndSourceServiceProvider.setLicenseServiceUrl(serverUrl + licenseUrl);
+    licenseAndSourceServiceProvider.setAssetSourceServiceUrl(serverUrl + assetSourceUrl);
+    tagServiceProvider.setServiceUrl(serverUrl + tagUrl);
+});
